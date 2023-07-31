2fc7afc9b41965eade53d305c89543501a75f3a3
SSSanga <ksa901125@gmail.com>
2023-07-31 17:14:25 +0900

    SA_PUSH_AUTHOR DEL 변경 ㅠㅠㅠㅠㅠㅠㅠ아 진짜 PAGINATION

--- src/main/java/com/project/project_nutrients/service/MembersService.java
+++ src/main/java/com/project/project_nutrients/service/MembersService.java
@@ -154,7 +154,7 @@ public class MembersService {
 
         HashMap result = new HashMap<>();
         result.put("deleteCount", this.deleteauths(dataMap));
-        result.putAll(this.memberList(dataMap));
+        result.putAll(this.membslistWithPaginations(dataMap));
         return result;
     }
 
