56ec24c196155a0438072a07e0a4c5f62b7a0d0b
jeongjiwooon <jjwanm@naver.com>
2023-10-10 12:45:58 +0900

    차트 띄움

--- src/main/java/com/project/project_nutrients/controller/ChartsController.java
+++ src/main/java/com/project/project_nutrients/controller/ChartsController.java
@@ -1,7 +1,6 @@
 package com.project.project_nutrients.controller;
 import java.util.ArrayList;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,30 +9,29 @@ import org.springframework.web.servlet.ModelAndView;
 import com.google.gson.Gson;
 
 import java.util.Arrays;
+
 @Controller
 @RequestMapping("/mem")
 public class ChartsController {
-    @Autowired
-    private Gson gson;
 
     @RequestMapping(value = "/charts/google_charts_scatter", method = RequestMethod.GET)  
     public ModelAndView edit(ModelAndView modelAndView) {
-        ArrayList datas = new ArrayList();
+        ArrayList<ArrayList<Object>> datas = new ArrayList<>();
         // ['Age', 'Weight'],[8, 12],[4, 5.5],[11, 14],[4, 5],[3, 3.5],[6.5, 7];
-        datas.add(Arrays.asList("Age", "Weight"));
-        datas.add(Arrays.asList(8, 12));
-        datas.add(Arrays.asList(4, 5.5));
-        datas.add(Arrays.asList(11, 14));
-        datas.add(Arrays.asList(4, 5));
-        datas.add(Arrays.asList(3, 3.5));
-        datas.add(Arrays.asList(6.5, 7));
-        ArrayList dataArray = new ArrayList<>();
-        dataArray.add(datas);
+        datas.add(new ArrayList<>(Arrays.asList("Age", "Weight")));
+        datas.add(new ArrayList<>(Arrays.asList(8, 12)));
+        datas.add(new ArrayList<>(Arrays.asList(4, 5.5)));
+        datas.add(new ArrayList<>(Arrays.asList(11, 14)));
+        datas.add(new ArrayList<>(Arrays.asList(4, 5)));
+        datas.add(new ArrayList<>(Arrays.asList(3, 3.5)));
+        datas.add(new ArrayList<>(Arrays.asList(6.5, 7)));
 
-        modelAndView.addObject("dataArray", gson.toJson(datas));
+        Gson gson = new Gson();
+        String jsonData = gson.toJson(datas);
+
+        modelAndView.addObject("dataArray", jsonData);
         
-        modelAndView.setViewName("WEB-INF/views/project/admin/charts_google_various.jsp");
+        modelAndView.setViewName("/WEB-INF/views/project/admin/charts_google_various.jsp");
         return modelAndView;
     }
-    
 }
